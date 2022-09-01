2cb306350a4847d47872b7975cf8ec2ead2df832
jd-Jason <102376407+jd-Jason@users.noreply.github.com>
2022-09-01 18:59:47 +0800

    Added Sidebar + working on css
    Quite messy need to clean up still.

---
+++ templates/participant_interface.js
@@ -0,0 +1,13 @@
+/*Side Bar Scripts */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidebar").style.padding = "8px 8px 8px 32px";
+  document.getElementById("mainSidebar").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidebar").style.padding = "0px 0px 0px 0px";
+  document.getElementById("mainSidebar").style.marginLeft= "0";
+
+}
