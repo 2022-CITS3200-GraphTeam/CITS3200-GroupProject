41321f029d864d10f9600a57789a09ca0a50ab6c
met4000 <nathsharwor@hotmail.com>
2022-09-16 15:17:44 +0800

    autoformat

--- components/admin_interface_main/main.js
+++ components/admin_interface_main/main.js
@@ -1,25 +1,26 @@
-function deleteRow(row,dd) {
-    var i = row.parentNode.parentNode.rowIndex;
-    console.log(dd)
-    document.getElementById(dd).deleteRow(i);
-  }
-  
-  function insRow(dd) {
-    console.log('hi');
-    var x = document.getElementById(dd);
-    var new_row = x.rows[1].cloneNode(true);
-    var len = x.rows.length;
-    new_row.cells[0].innerHTML = len;
-  
-    var inp1 = new_row.cells[1].getElementsByTagName('input')[0];
-    inp1.id += len;
-    inp1.value = '';
-    var inp2 = new_row.cells[2].getElementsByTagName('input')[0];
-    inp2.id += len;
-    inp2.value = '';
-    x.appendChild(new_row);
-  }
-  function generateGraph(){
+function deleteRow(row, dd) {
+  var i = row.parentNode.parentNode.rowIndex;
+  console.log(dd)
+  document.getElementById(dd).deleteRow(i);
+}
+
+function insRow(dd) {
+  console.log('hi');
+  var x = document.getElementById(dd);
+  var new_row = x.rows[1].cloneNode(true);
+  var len = x.rows.length;
+  new_row.cells[0].innerHTML = len;
+
+  var inp1 = new_row.cells[1].getElementsByTagName('input')[0];
+  inp1.id += len;
+  inp1.value = '';
+  var inp2 = new_row.cells[2].getElementsByTagName('input')[0];
+  inp2.id += len;
+  inp2.value = '';
+  x.appendChild(new_row);
+}
+
+function generateGraph() {
   const data = {
     labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     datasets: [{
@@ -73,4 +74,4 @@ function deleteRow(row,dd) {
     document.getElementById('myChart'),
     config
   );
-  }
+}
