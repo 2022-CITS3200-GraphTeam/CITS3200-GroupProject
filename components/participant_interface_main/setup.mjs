c75e4acf803c8b0d5e1f900cfef5e68c2027a2d3
met4000 <nathsharwor@hotmail.com>
2022-09-23 21:30:17 +0800

    remove temp event

--- components/participant_interface_main/setup.mjs
+++ components/participant_interface_main/setup.mjs
@@ -2,8 +2,3 @@ import { handleInjectionMessage, setAnswer } from "./iframe_coms.mjs";
 
 // handle messages from the Qualtrics injection
 window.addEventListener("message", handleInjectionMessage);
-
-// temp
-document.getElementById("submitButton").addEventListener("click", () => {
-  setAnswer("test");
-});
