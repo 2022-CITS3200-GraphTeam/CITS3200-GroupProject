cf249b5705c3c0791409c237531a4bfd58c115cc
met4000 <nathsharwor@hotmail.com>
2022-09-27 17:24:36 +0800

    modify generateGraph to loadGraph - loads a graph object

--- components/participant_interface_main/participant_interface.js
+++ components/participant_interface_main/participant_interface.js
@@ -7,22 +7,22 @@ var btn = document.getElementById("helpButton");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-let graphData, myChart;
+let graphChart;
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 window.onerror = function (e) {
-    alert(e);
+  alert(e);
 }
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
@@ -38,65 +38,56 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("mySidebar").style.padding = "0px 0px 0px 0px";
-  document.getElementById("mainSidebar").style.marginLeft= "0";
+  document.getElementById("mainSidebar").style.marginLeft = "0";
 
 }
-function updateSection(){
-//On update click > Grab integer value from input box > update data value in graph
-const sectionValue = document.getElementById("integerValue").value;
-const sectionName = document.getElementById("sectionName").value;
-//console.log(myChart.data.labels[1]);
-const sectionIndex = myChart.data.labels.indexOf(sectionName);
-console.log(sectionIndex);
-myChart.data.datasets[0].data[sectionIndex] = sectionValue;
-myChart.update();
+function updateSection() {
+  //On update click > Grab integer value from input box > update data value in graph
+  const sectionValue = document.getElementById("integerValue").value;
+  const sectionName = document.getElementById("sectionName").value;
+  //console.log(myChart.data.labels[1]);
+  const sectionIndex = graphChart.data.labels.indexOf(sectionName);
+  console.log(sectionIndex);
+  graphChart.data.datasets[0].data[sectionIndex] = sectionValue;
+  graphChart.update();
 }
 
 //Initial attempt at understanding how to update values - high level idea experimenting
 //myChart.data.labels[index] =  sectionName;
 //myChart.data.datasets[datasetIndex].data[index] =  sectionValue;
 
-function generateGraph() {
-  graphData = {
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    datasets: [{
-      label: 'Weekly Sales',
-      data: [18, 12, 6, 9, 12, 3, 9],
-      backgroundColor: [
-        'rgba(255, 26, 104, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(0, 0, 0, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 26, 104, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-        'rgba(0, 0, 0, 1)'
-      ],
-      borderWidth: 1,
-      dragData: true,
-    }]
-  };
-
-  // config 
-  const config = {
+// ! TEMP - exists to load the graph for testing; should be removed when the qualtrics injection calls the graph generation
+function generateGraphPreset() {
+  let chartjsObj = {
     type: 'bar',
-    data: graphData,
+    data: {
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      datasets: [{
+        label: 'Weekly Sales',
+        data: [18, 12, 6, 9, 12, 3, 9],
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(0, 0, 0, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(0, 0, 0, 1)'
+        ],
+        borderWidth: 1,
+        dragData: true,
+      }]
+    },
     options: {
-      plugins: {
-        dragData: {
-          onDrag: (event, datasetIndex, index, value) => {
-            dragHandler(datasetIndex, index, value)
-          }
-        }
-      },
       scales: {
         y: {
           beginAtZero: true
@@ -105,40 +96,53 @@ function generateGraph() {
     }
   };
 
+  loadGraph(chartjsObj);
+}
+
+/** */
+function loadGraph(graphObj) {
+  // update the input when a drag occurs
+  if (graphObj.options === undefined) graphObj.options = {};
+  if (graphObj.options.plugins === undefined) graphObj.options.plugins = {};
+  if (graphObj.options.plugins.dragData === undefined) graphObj.options.plugins.dragData = {};
+  graphObj.options.plugins.dragData.onDrag = (event, datasetIndex, index, value) => {
+    dragHandler(datasetIndex, index, value)
+  };
+
   //pintpointing the chart, so that the click understands the canvs tag
   const ctx = document.getElementById('myChart');
   // render init block
-  myChart = new Chart(
+  graphChart = new Chart(
     document.getElementById('myChart'),
-    config
+    graphObj
   );
 
   //Function that understands the clicking event - testing how to properly use this
-  function clickHandler(click){
-      const points = myChart.getElementsAtEventForMode(click, 'nearest', 
-          { intersect: true }, true);
+  function clickHandler(click) {
+    const points = graphChart.getElementsAtEventForMode(click, 'nearest',
+      { intersect: true }, true);
 
-      if (points.length){
-          const firstPoint = points[0];
-          const value = myChart.data.datasets[firstPoint.datasetIndex].
-              data[firstPoint.index];
-          const name = myChart.data.labels[firstPoint.index];
-
-          //console.log(data.labels)
-          document.getElementById("sectionName").value = name;
-          document.getElementById("integerValue").value = value;
-      }
-  }
-  ctx.onclick = clickHandler;
-
-  //Function that understands the clicking event - testing how to properly use this
-  function dragHandler(datasetIndex, index, value){
-      const name = myChart.data.labels[index];
+    if (points.length) {
+      const firstPoint = points[0];
+      const value = graphChart.data.datasets[firstPoint.datasetIndex].
+        data[firstPoint.index];
+      const name = graphChart.data.labels[firstPoint.index];
 
       //console.log(data.labels)
       document.getElementById("sectionName").value = name;
       document.getElementById("integerValue").value = value;
     }
+  }
+  ctx.onclick = clickHandler;
+
+  //Function that understands the clicking event - testing how to properly use this
+  function dragHandler(datasetIndex, index, value) {
+    const name = graphChart.data.labels[index];
+
+    //console.log(data.labels)
+    document.getElementById("sectionName").value = name;
+    document.getElementById("integerValue").value = value;
+  }
 }
 
 
