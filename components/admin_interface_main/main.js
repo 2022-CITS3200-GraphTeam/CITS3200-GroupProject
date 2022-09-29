cd98f09818230ac47c812254f8cbb070c9bc0a66
WayneLJX <906162399@qq.com>
2022-09-29 11:00:32 +0800

    Help Button + Popup modal

--- components/admin_interface_main/main.js
+++ components/admin_interface_main/main.js
@@ -98,3 +98,33 @@ function updateGraph() {
   myChart.data.datasets[0].data = getColValues();
   myChart.update();
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("helpButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+let graphChart;
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+window.onerror = function (e) {
+  alert(e);
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
