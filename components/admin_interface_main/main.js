54b9121448d37b14828a69d01e21f27dc4b03a83
met4000 <nathsharwor@hotmail.com>
2022-10-16 17:11:03 +0800

    remove hardcoded min

--- components/admin_interface_main/main.js
+++ components/admin_interface_main/main.js
@@ -1,17 +1,17 @@
 function makeColRowHTML(n) {
   return `
 <td><input class="nameInput" oninput="updateGraph()" value="Column ${n}" size=20 type="text"></td>
-<td><input class="valueInput" onchange="roundToStepSize(this)" oninput="updateGraph()" value="${n}" min="0" size=10 type="number" step="getStepSize()"></td>
+<td><input class="valueInput" onchange="roundToStepSize(this)" oninput="updateGraph()" value="${n}" size=10 type="number" step="getStepSize()"></td>
 <td><input class="colourInput" id="colourInput" type="color" oninput="updateGraph()" value="#0072D0"></td>
 <td><input class="deleteButton" type="button" value="Delete" onclick="deleteRow(this, 'colTable');updateGraph()"></td>
 `;
 }
 
-function minErrorMessage(){
-  if(getStepSize() != 0 && Decimal(getScaleMin()).modulo(getStepSize()) != 0){
+function minErrorMessage() {
+  if (getStepSize() != 0 && Decimal(getScaleMin()).modulo(getStepSize()) != 0) {
     document.getElementById("errorText").style.display = "block";
   }
-  else{
+  else {
     document.getElementById("errorText").style.display = "none";
   }
 }
@@ -88,11 +88,11 @@ function getScaleIncrement() {
   var testScaleIncrement = document.getElementById("scaleIncrement").value;
   return Math.round(testScaleIncrement * 100) / 100;
 }
-function getStepSize(){
+function getStepSize() {
   var stepSize = document.getElementById('stepSize').value;
   return stepSize;
 }
-function roundValueToStepSize(value){
+function roundValueToStepSize(value) {
   var decimalPlaces = Decimal(getStepSize()).dp();
   value = Decimal(value).toDecimalPlaces(decimalPlaces);
   return value;
@@ -174,7 +174,7 @@ function updateGraph() {
   myChart.config._config.options.scales.y.min = getScaleMin();
   myChart.config._config.options.scales.y.max = getScaleMax();
   myChart.config._config.options.scales.y.ticks.stepSize = getScaleIncrement();
-  myChart.options.plugins.dragData.round = Math.log10(1/getStepSize());
+  myChart.options.plugins.dragData.round = Math.log10(1 / getStepSize());
 
   myChart.data.datasets[0].backgroundColor = getColour();
   myChart.data.datasets[0].borderColor = getColour();
@@ -185,36 +185,36 @@ function updateGraph() {
   document.getElementById("currentSum").innerHTML = roundValueToStepSize(graphValueSum);
 
   myChart.update();
-  var values = document.getElementsByClassName('valueInput'); 
+  var values = document.getElementsByClassName('valueInput');
   updateStepSize(values);
   updateMinValues(values);
   //for each value automatically updates the values and rounds them if the checkbox is ticked.
-  if(document.getElementById("roundToStepSizeButton").checked && values[x] % getStepSize()){
-    for(var x in values){
+  if (document.getElementById("roundToStepSizeButton").checked && values[x] % getStepSize()) {
+    for (var x in values) {
       roundToStepSize(values[x]);
     }
   }
 }
 //Updates the minimum values and changes the value if it is less then the new minimum
-function updateMinValues(values){
-  for(var x in values){
-    if(values[x].value < getScaleMin()){
+function updateMinValues(values) {
+  for (var x in values) {
+    if (values[x].value < getScaleMin()) {
       values[x].value = getScaleMin();
     }
-    }
-    values[x].min = getScaleMin();
+  }
+  values[x].min = getScaleMin();
 }
 //Updates the step size for all the value boxes so the first click on the up arrow will 
 //use the new correct step size
-function updateStepSize(values){
-  for (var x in values){
+function updateStepSize(values) {
+  for (var x in values) {
     values[x].step = getStepSize();
   }
 }
 
 function dragHandler(datasetIndex, index, value) {
   const name = myChart.data.labels[index];
-  if(value>3.3){
+  if (value > 3.3) {
     var x = getDecimalPlaces(getStepSize());
   }
   document.getElementsByClassName("valueInput")[index].value = value.toFixed(getDecimalPlaces(getStepSize()));
@@ -226,7 +226,7 @@ function getChartObj() {
   return myChart.config._config;
 }
 
-function returnTutorial(){
+function returnTutorial() {
   return document.getElementById('saveTutorial');
 }
 
@@ -237,7 +237,7 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("helpButton");
 
 var parModal = document.getElementById("participantModal");
-var parModalbtn = document.getElementById("parModalButton"); 
+var parModalbtn = document.getElementById("parModalButton");
 
 window.onerror = function (e) {
   alert(e);
@@ -248,34 +248,33 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-  else if (event.target == parModal){
+  else if (event.target == parModal) {
     parModal.style.display = "none"
   }
 }
 
-function openModal(modalName){
+function openModal(modalName) {
   var modal = document.getElementById(modalName)
   modal.style.display = "block";
 }
 
-function closeModal(modalName){
+function closeModal(modalName) {
   var modal = document.getElementById(modalName)
   modal.style.display = "none";
 }
 
 
-function roundToStepSize(column){
+function roundToStepSize(column) {
   column.step = getStepSize();
   var decimalPlaces = Decimal(getStepSize()).dp();
-  if(document.getElementById("roundToStepSizeButton").checked){
+  if (document.getElementById("roundToStepSizeButton").checked) {
     column.value = roundValueToStepSize(column.value)
   }
 }
-function getDecimalPlaces(number)
-{
+function getDecimalPlaces(number) {
   var char_array = number.toString().split(""); // split every single char
   var not_decimal = char_array.lastIndexOf(".");
-  return (not_decimal<0)?0:char_array.length - (not_decimal + 1);
+  return (not_decimal < 0) ? 0 : char_array.length - (not_decimal + 1);
 }
 
 document.getElementById("addRuleButton").addEventListener("click", () => document.getElementById("submitButton").scrollIntoView());
